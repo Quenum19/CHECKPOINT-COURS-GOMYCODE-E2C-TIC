ff63fe56efe88a1f3bd94dda57c865cc5ae40b79
Sio Romuald Quenum <sioquenum75@gmail.com>
2025-03-07 18:59:31 +0000

    Add new doc

---
+++ API Checkpoint/user-list-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import UserList from './UserList'; // Importation de UserList
+
+function App() {
+  return (
+    <div className="App">
+      <UserList /> {/* Utilisation du composant UserList */}
+    </div>
+  );
+}
+
+export default App;
